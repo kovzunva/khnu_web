314bdb5d8e467bc789837ff56a6593f857b86afa
vika.k <vika.k@twincore.net>
2024-11-27 17:05:51 +0200

    changes

---
+++ public/dovidka/zoom_pageinfo.js
@@ -0,0 +1,14 @@
+pageinfo = [[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null]];
+pagedata = [ ["chapter_2.htm","Жанри фентезі","Жанри фентезі Top Previous Next В жанрі фентезі існує безліч піджанрів та підкатегорій, оскільки ця область дозволяє творити різноманітні світи та...",""],
+["introduction.htm","Вступ","Вступ Top Next Ви переглядаєте довідкові матеріали до експертної системи \"Підбір жанру фентезі\". Експертна система Фентезі This help file was crea...",""],
+["new_topic.htm","New topic","New topic Top Previous Next Enter topic text here. This help file was created with an unregistered evaluation copy of Help &#38; Manual. &#169; EC Software...",""],
+["overview.htm","Overview","Overview Top Previous Next Enter topic text here. This help file was created with an unregistered evaluation copy of Help &#38; Manual. &#169; EC Software....",""],
+["second_topic.htm","Фентезі","Фентезі Top Previous Next Фентезі (Fantasy) - це жанр художньої літератури і кінематографу, який характеризується наявністю елементів, які перевищ...",""],
+["sub_chapter_2_1.htm","Sub chapter 2.1","Sub chapter 2.1 Top Previous Next Enter topic text here. This help file was created with an unregistered evaluation copy of Help &#38; Manual. &#169; EC So...",""],
+["welcome_topic.htm","Експертна система","Експертна система Top Previous Next Експертна система для підбору жанру фентезі на основі тестування призначена для особистих рекомендацій користу...",""]];
